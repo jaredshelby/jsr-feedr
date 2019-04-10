e4d87b99d52270ae412a7982464f826a0c8cef80
jaredshelby <jaredshelby@Jareds-MacBook-Pro.local>
2019-04-10 14:46:59 -0700

    DOM targeting

--- js/app.js
+++ js/app.js
@@ -1,8 +1,21 @@
 // Please add all Javascript code to this file.
 
-//DOM Elements
+// Grabbing DOM Elements
 const search = document.querySelector('#search');
+const popUp = document.querySelector('#popUp');
+const closePopUp = document.querySelector('.closePopUp');
+const navLink = document.querySelectorAll('nav ul li ul li a');
 
 search.addEventListener('click', function(){ 
 	search.classList.toggle('active');
+});
+
+closePopUp.addEventListener('click', function(){ 
+	popUp.classList.add('hidden');
+});
+
+navLink.forEach(function(elem) {
+    elem.addEventListener('click', function() {
+        popUp.classList.remove('hidden');
+    });
 });
