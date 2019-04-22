de7ab99cfe3f0f7bec2ad01d2c80dc642bb7bb37
jaredshelby <jaredshelby@Jareds-MacBook-Pro.local>
2019-04-22 14:52:49 -0700

    Bulk of work done

--- js/app.js
+++ js/app.js
@@ -1,74 +1,121 @@
-// Please add all Javascript code to this file.
-
 // Grabbing DOM Elements
-const search = document.querySelector('#search');
+const logo = document.querySelector('.logo');
+const search = document.querySelector('#search a');
 const popUp = document.querySelector('#popUp');
 const closePopUp = document.querySelector('.closePopUp');
-const navLink = document.querySelectorAll('nav ul li ul li a');
-const featImage = document.querySelector('.featuredImage img');
-const articleTitle = document.querySelector('.articleContent a h3');
-const articleDesc = document.querySelector('.articleContent h6');
-const impCount = document.querySelector('.impressions');
+// const navLink = document.querySelectorAll('nav ul li ul li a');
+const cnnLink = document.querySelector('.cnn-link');
+const espnLink = document.querySelector('.espn-link');
+const nytimesLink = document.querySelector('.nytimes-link');
+const mainSection = document.querySelector('#main');
+const	title = document.querySelector('h3');
 
 // DOM and UI Interactions
 search.addEventListener('click', function(){ 
-	search.classList.toggle('active');
+	document.querySelector('#search').classList.toggle('active');
 });
 
 closePopUp.addEventListener('click', function(){ 
 	popUp.classList.add('hidden');
 });
 
-navLink.forEach(function(elem) {
-    elem.addEventListener('click', function() {
-        popUp.classList.remove('hidden');
-    });
-});
-
 // API variables
 const xhr = new XMLHttpRequest();
-const NEWSAPI_KEY = '39cd29a6a549437caac9b3b0c71c7c4c';
+const url = `https://newsapi.org/v2/top-headlines?sources=`;
+let apiKey = `&apiKey=39cd29a6a549437caac9b3b0c71c7c4c`;
 let cleanData = {};
-let url = `https://newsapi.org/v2/top-headlines?` +
-          `country=us&` +
-          `apiKey=${NEWSAPI_KEY}`;
-
 
 // API Work
-xhr.open('GET', url);
-xhr.send();
-xhr.onload = onSuccess;
+function APITime(source) {
+	xhr.open('GET', url + source + apiKey);
+	xhr.send();
+	xhr.onload = onSuccess;
+}
 
 function onSuccess() {
+	popUp.classList.add('hidden');
 	cleanData = JSON.parse(xhr.responseText);
-  	console.log(cleanData);
-  	changeHTML(cleanData);
+  changeHTML(cleanData);
 }
 
+// Adding and formatting articles
 function changeHTML(data) {
-	var artArray = data.articles;
-	artArray.forEach(function(article, index) {
-	  featImage.src = data.articles[index].urlToImage;
-	  articleTitle.innerHTML = data.articles[index].title;
-	  articleDesc.innerHTML = data.articles[index].description;
-	  impCount.innerHTML = data.articles[index].publishedAt;
+	mainSection.innerHTML = '';
+	let moreInfo = [];
+	  for(let i = 0; i < data.articles.length; i++) {
+			let imgSRC = data.articles[i].urlToImage;
+			let articleTitle = data.articles[i].title;
+			let articleDesc = data.articles[i].description;
+			let impCount = data.articles[i].publishedAt.substring(0, 10); // Only want date, so using substring to slice the end off 
+
+			let articleElement = document.createElement('article');
+			articleElement.className = 'article';
+
+			// Putting API content in HTML format
+			articleElement.innerHTML = `<section class="featuredImage">
+											              <img src="${imgSRC}" alt="" />
+											            </section>
+											            <section class="articleContent">
+										                <a href="#"><h3>${articleTitle}</h3></a>
+										                <h6>${articleDesc}</h6>
+											            </section>
+											            <section class="impressions">
+											              ${impCount}
+											            </section>
+											            <div class="clearfix"></div>`;
+
+			// Adding article to section
+			mainSection.appendChild(articleElement);
+			// Links to open pop up
+			moreInfo.push(articleElement.querySelector('.articleContent a'));
+	}
+	seeMore(moreInfo, cleanData);
+}
+
+// Article content popup
+
+function seeMore(buttonArray, data) {
+	buttonArray.forEach(function(current, index) {
+    current.addEventListener('click', function() {
+        popUp.classList.remove('hidden');
+        popUp.classList.remove('loader');
+        popUp.querySelector('.container h1').innerText = data.articles[index].title;
+        if (data.articles[index].url == 'null') {
+        	popUp.querySelector('.container h3').innerText = '';
+        } else {
+        	popUp.querySelector('.container h3').innerText = data.articles[index].author;
+      	}
+        popUp.querySelector('.container p').innerText = data.articles[index].description;
+        popUp.querySelector('.container img').src = data.articles[index].urlToImage;
+        popUp.querySelector('.popUpAction').href = data.articles[index].url;
+    });
 	});
 }
 
-function changeHTML(data) {
-	var artArray = data.articles;
-	artArray.forEach(function(article, index) {
-	  featImage.src = data.articles[index].urlToImage;
-	  articleTitle.innerHTML = data.articles[index].title;
-	  articleDesc.innerHTML = data.articles[index].description;
-	  impCount.innerHTML = data.articles[index].publishedAt;
-	  console.log(index);
-	});
-}
+// Clicking on Logo loads all three sources
+logo.addEventListener('click', function() {
+        popUp.classList.remove('hidden');
+        APITime('cnn,espn,the-new-york-times');
+});
 
+// Source drop down - Should probably think of way to make this more DRY and not repeat code three times
+cnnLink.addEventListener('click', function() {
+        popUp.classList.remove('hidden');
+        APITime('cnn');
+});
 
+espnLink.addEventListener('click', function() {
+        popUp.classList.remove('hidden');
+        APITime('espn');
+});
 
+nytimesLink.addEventListener('click', function() {
+        popUp.classList.remove('hidden');
+        APITime('the-new-york-times');
+});
 
+// Initial Call 
+APITime('cnn,espn,the-new-york-times');
 
 
 
